b22b7608c0c7107504191b55c27f92259bebfcda
azariel <frost.qc@gmail.com>
2026-02-28 15:29:49 -0500

    test

---
+++ CohesiveRP.UI.WebFrontend/src/main/components/modules/settings/SettingsComponent.ts
@@ -0,0 +1,48 @@
+import styles from "./SettingsComponent.module.css";
+import { useEffect, useState, useRef  } from "react";
+
+// Backend webapi
+import { getFromServerApiAsync } from "../../../../utils/http/HttpRequestHelper";
+import type { ServerApiExceptionResponseDto } from "../../../../ResponsesDto/Exceptions/ServerApiExceptionResponseDto";
+import type { SettingsResponseDto } from "../../../../ResponsesDto/settings/SettingsResponseDto"
+
+/* Store */
+//import type { SharedContextSettings } from "../../../../store/SharedContextSettings";
+
+export default function SettingsComponent() {
+  //sharedContext<SharedContextSettings>();
+  const didComponentMountAlready = useRef(false);
+  const [setSettings] = useState<SettingsResponseDto>();
+
+  useEffect(() => {
+    if (didComponentMountAlready.current)
+      return;
+
+    didComponentMountAlready.current = true;
+
+    const fetchData = async () => {
+      try {
+        const response:SettingsResponseDto | null = await getFromServerApiAsync<SettingsResponseDto>("api/settings");
+
+        let serverApiException = response as ServerApiExceptionResponseDto | null;
+        if(!response || response.code != 200 || serverApiException?.message) {
+          console.error(`Call to fetch settings failed. [${serverApiException}] [${JSON.stringify(serverApiException)}]`);
+          return;
+        }
+
+        setSettings(response);
+        console.log(`Settings fetched successfully.`);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <main className={styles.settingsComponent}>
+      
+    </main>
+  );
+}
