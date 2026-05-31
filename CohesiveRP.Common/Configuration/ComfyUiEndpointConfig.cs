5a079821a04ab0bbd6dfc2f4a5e26c396ca20f60
Azariel <frost.qc@gmail.com>
2026-05-31 06:05:14 -0400

    up

--- CohesiveRP.Common/Configuration/ComfyUiEndpointConfig.cs
+++ CohesiveRP.Common/Configuration/ComfyUiEndpointConfig.cs
@@ -2,7 +2,7 @@
 {
     public class ComfyUiEndpointConfig
     {
-        public string BaseUrl { get; set; } = "http://localhost:8188";
+        public string BaseUrl { get; set; } = "http://192.168.0.237:8188";
         public int ExecutionTimeoutMinutes { get; set; } = 10;
     }
 }
